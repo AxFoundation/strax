e5d8c5f4025fa610177f0e2ce21b646a6f3833a4
Jelle Aalbers <jelleaalbers@gmail.com>
2018-03-30 19:20:47 +0200

    Refactor for saner chunking

--- strax/__init__.py
+++ strax/__init__.py
@@ -1,17 +1,20 @@
+# flake8: noqa
 __version__ = '0.0.1'
 
-# These import *'s are benign, all files define __all__
+# Glue the package together
+# See https://www.youtube.com/watch?v=0oTh1CXRaQ0 if this confuses you
 
-from .dtypes import *               # noqa
-from .io import *                   # noqa
-from strax.processing.data_reduction import *       # noqa
-from strax.processing.pulse_processing import *     # noqa
-from strax.processing.peak_building import *        # noqa
-from strax.processing.peak_splitting import *       # noqa
-from strax.processing.peak_properties import *      # noqa
-from .utils import *                # noqa
-from .plugin import *               # noqa
+from .dtypes import *
+from .io import *
+from .utils import *
+from .plugin import *
+
+from strax.processing.data_reduction import *
+from strax.processing.pulse_processing import *
+from strax.processing.peak_building import *
+from strax.processing.peak_splitting import *
+from strax.processing.peak_properties import *
 
 from strax.external import pax_interface
-from . import io_chunked            # noqa
-from . import chunk_arrays          # noqa
+from . import io_chunked
+from . import chunk_arrays
