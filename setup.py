75c833fa92e31995aab679539ccdcfeaf55ee881
Christopher Tunnell <tunnell@uchicago.edu>
2018-04-04 08:53:54 -0500

    Avoid repeating requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ try:
 except ImportError:
     from distutils.core import setup
 
-requires = 'numpy pandas numba blosc zstd tqdm dill'.split()
-
+def req_file(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    return [x.strip() for x in content] 
+    
 setup(name='strax',
       version='0.0.1',
       description='Streaming analysis for XENON',
@@ -12,6 +16,6 @@ setup(name='strax',
       author_email='j.aalbers@uva.nl',
       url='https://github.com/jelleaalbers/strax',
       setup_requires=['pytest-runner'],
-      install_requires=requires,
+      install_requires=req_file('requirements.txt'),
       tests_require=requires + ['pytest', 'boltons', 'hypothesis'],
       packages=['strax'])
