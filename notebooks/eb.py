74685d5be7866c9f19cb675092ed90ede77b6a29
Jelle Aalbers <jelleaalbers@gmail.com>
2018-04-24 18:06:09 +0200

    Separate processors from core, inheritance

--- notebooks/eb.py
+++ notebooks/eb.py
@@ -1,7 +1,4 @@
-# import logging
-# logging.basicConfig(level=logging.DEBUG,
-#                     format='{name} in {threadName} at {asctime}: {message}', style='{')
-# logging.getLogger().setLevel(logging.DEBUG)
+import logging
 import subprocess
 import argparse
 import os
@@ -13,6 +10,21 @@ from concurrent.futures import ProcessPoolExecutor
 
 import numpy as np
 import strax
+
+logging.basicConfig(
+   level=logging.INFO,
+   format='{name} in {threadName} at {asctime}: {message}', style='{')
+log = logging.getLogger()
+
+
+def du(path):
+    """disk usage in human readable format (e.g. '2,1GB')"""
+    try:
+        return subprocess.check_output([
+            'du','-sh', path]).split()[0].decode('utf-8')
+    except subprocess.CalledProcessError:
+        return float('nan')
+
 
 parser = argparse.ArgumentParser(
     description='XENONnT eventbuilder prototype')
@@ -43,7 +55,6 @@ mystrax = strax.Strax(storage=[strax.FileStorage(data_dirs=[out_dir])])
 mystrax.register_all(strax.xenon.plugins)
 low_proc = mystrax.simple_chain(run_id, source='records', target='peaks')
 
-
 def build(chunk_i):
     print(f"\t{chunk_i}: started job")
 
@@ -62,18 +73,16 @@ def build(chunk_i):
     print(f"\t{chunk_i}: low-level processing done")
     return peaks
 
+log.debug("Created low-level processor")
 
 ##
 # High-level processing: build everything from peaks
 ##
-class PeakFeeder(strax.ReceiverPlugin):
-    provides = 'peaks'
-    dtype = strax.peak_dtype(n_channels=len(strax.xenon.common.to_pe))
-    save_when = strax.SaveWhen.NEVER
-
-
+@mystrax.register
 class TellS1s(strax.Plugin):
-    """Stupid plugin to report number of S1s found"""
+    """Stupid plugin to report number of S1s found
+    should be removed soon
+    """
     dtype = [('n_s1s', np.int16)]
     counter = 0
     parallel = False
@@ -89,35 +98,29 @@ class TellS1s(strax.Plugin):
         r[0]['n_s1s'] = n_s1s
         return r
 
-mystrax.register(PeakFeeder)
-mystrax.register(TellS1s)
-high_proc = mystrax.online(run_id, 'tell_s1s')
+# TODO: make option to NOT save? This is too ugly:
+mystrax._plugin_class_registry['peaks'].save_when = strax.SaveWhen.NEVER
+
+high_proc = mystrax.in_background(run_id, 'tell_s1s', sources=['peaks'])
 
 
 def finish(future, chunk_i):
     high_proc.send('peaks', chunk_i, future.result())
+    if not args.erase:
+        done_chunks.add(chunk_i)
     pending_chunks.remove(chunk_i)
-    done_chunks.add(chunk_i)
+
 
 
 ##
 # Main loop
 ##
 
-
-def du(path):
-    """disk usage in human readable format (e.g. '2,1GB')"""
-    try:
-        return subprocess.check_output([
-            'du','-sh', path]).split()[0].decode('utf-8')
-    except subprocess.CalledProcessError:
-        return float('nan')
-
-
 done = False
 pending_chunks = set()
-done_chunks = set()         # Hmm, clean up once in a while?
-with ProcessPoolExecutor(max_workers=args.n) as pool:
+done_chunks = set()         # Only used if not erasing data (unbounded memory)
+high_proc.log.setLevel(logging.DEBUG)
+with low_proc, high_proc, ProcessPoolExecutor(max_workers=args.n) as pool:
     while not done:
         print(f'{du(in_dir)} compressed data in buffer, '
               f'{len(pending_chunks)} chunks pending, '
@@ -125,25 +128,22 @@ with ProcessPoolExecutor(max_workers=args.n) as pool:
         if os.path.exists(in_dir + '/THE_END'):
             done = True
 
-        chunks = [int(x)
-                  for x in sorted(os.listdir(in_dir))
-                  if not x.endswith('_temp')
-                  and os.path.isdir(in_dir + '/' + x)]
-        chunks = [c for c in chunks
-                  if c not in pending_chunks
-                  and c not in done_chunks]
-        if not len(chunks):
+        to_submit = []
+        for x in glob.glob(in_dir + '/*/'):
+            c = int(x.split('/')[-2])
+            if c not in pending_chunks and c not in done_chunks:
+                to_submit.append(c)
+
+        if not len(to_submit):
             print("\t\tNothing to submit, sleeping")
             time.sleep(2)
             continue
 
-        for chunk_i in chunks:
+        for chunk_i in to_submit:
             print(f"\t{chunk_i}: job submitted")
-            # Submit new job
             f = pool.submit(build, chunk_i)
-            f.add_done_callback(partial(finish, chunk_i=int(chunk_i)))
+            f.add_done_callback(partial(finish,
+                                        chunk_i=int(chunk_i)))
             pending_chunks.add(chunk_i)
 
-print("Run done and all jobs submitted. Waiting for final results.")
-low_proc.close()
-high_proc.close()
+    print("Run done and all jobs submitted. Waiting for final results.")
