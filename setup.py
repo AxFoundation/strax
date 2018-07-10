33b8f4e682e38a7351818f68298501591590280f
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-07-10 17:25:42 +0300

    Update setup.py following packaging guidelines (#43)
    * Update setup.py following packaging guidelines
    
    Read packaging.python.org after suggestion from LHCb software librarian.  Made the following changes:
    
    *  Find packages using:  `setuptools.find_packages()`
    * Dropped disttools
    * Marked beta since I think we're there in next release
    * Added `Intended Audience :: Science/Research` and `Programming Language :: Python :: Implementation :: CPython` and `Topic :: Scientific/Engineering :: Physics`
    * Changed spacing
    
    * setuptools fix

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
 # Get requirements from requirements.txt, stripping the version tags
 with open('requirements.txt') as f:
@@ -14,36 +11,36 @@ with open('README.md') as file:
 with open('HISTORY.md') as file:
     history = file.read()
 
-setup(name='strax',
-      version='0.2.0',
-      description='Streaming analysis for XENON',
-      author='Jelle Aalbers',
-      author_email='j.aalbers@uva.nl',
-      url='https://github.com/AxFoundation/strax',
-      setup_requires=['pytest-runner'],
-      install_requires=requires,
-      tests_require=requires + ['pytest',
-                                'boltons',
-                                'hypothesis'],
-      long_description=readme + '\n\n' + history,
-      extras_require={
-          'docs': ['sphinx',
-                   'sphinx_rtd_theme',
-                   'nbsphinx',
-                   'recommonmark'],
-          'xenon': ['keras'
-                    'tensorflow',
-                    'scipy']
-      },
-      long_description_content_type="text/markdown",
-      packages=['strax',
-                'strax.processing',
-                'strax.storage',
-                'strax.xenon'],
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',],
-      zip_safe = False,
-    )
+setuptools.setup(name='strax',
+                 version='0.2.0',
+                 description='Streaming analysis for XENON',
+                 author='Jelle Aalbers',
+                 author_email='j.aalbers@uva.nl',
+                 url='https://github.com/AxFoundation/strax',
+                 setup_requires=['pytest-runner'],
+                 install_requires=requires,
+                 tests_require=requires + ['pytest',
+                                           'boltons',
+                                           'hypothesis'],
+                 long_description=readme + '\n\n' + history,
+                 extras_require={
+                     'docs': ['sphinx',
+                              'sphinx_rtd_theme',
+                              'nbsphinx',
+                              'recommonmark'],
+                     'xenon': ['keras'
+                               'tensorflow',
+                               'scipy']
+                 },
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: BSD License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.6',
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: Implementation :: CPython',
+                     'Topic :: Scientific/Engineering :: Physics',
+                 ],
+                 zip_safe = False)
