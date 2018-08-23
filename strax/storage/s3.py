ec1532e4982e79bcb97962a4a9ed8ec474dce9f5
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-23 11:52:36 +0200

    Various lint fixes
    Related to changing variable names relative to what they overload

--- strax/storage/s3.py
+++ strax/storage/s3.py
@@ -137,17 +137,17 @@ class S3Backend(strax.StorageBackend):
                                service_name='s3')
         self.kwargs = kwargs  # Used later for setting up Saver
 
-    def get_metadata(self, strax_unique_key):
+    def get_metadata(self, key):
         # Grab metadata object from S3 bucket
         result = self.s3.get_object(Bucket=BUCKET_NAME,
-                                    Key=f'{strax_unique_key}/metadata.json')
+                                    Key=f'{key}/metadata.json')
         # Then read/parse it into ASCII
         text = result["Body"].read().decode()
 
         # Before returning dictionary
         return json.loads(text)
 
-    def _read_chunk(self, dirname, chunk_info, dtype, compressor):
+    def _read_chunk(self, backend_key, chunk_info, dtype, compressor):
         with tempfile.SpooledTemporaryFile() as f:
             self.s3.download_fileobj(Bucket=BUCKET_NAME,
                                      Key=chunk_info['key_name'],
@@ -157,7 +157,7 @@ class S3Backend(strax.StorageBackend):
                                    dtype=dtype,
                                    compressor=compressor)
 
-    def _saver(self, dirname, metadata, meta_only=False):
+    def _saver(self, key, metadata, meta_only=False):
         return S3Saver(dirname,
                        metadata=metadata,
                        meta_only=meta_only,
