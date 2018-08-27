592d5f9f0856296fd6b90487813648adf1a79743
JelleAalbers <jelleaalbers@gmail.com>
2018-08-27 15:16:15 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='strax',
-                 version='0.3.0',
+                 version='0.4.0',
                  description='Streaming analysis for XENON',
                  author='Jelle Aalbers',
                  author_email='j.aalbers@uva.nl',
