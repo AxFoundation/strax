6cf3d9d2519648f8e625fdbc07e1c18eb3141762
xedaq <coderdj@gmail.com>
2019-05-10 00:20:15 +0200

    Fix pep8 errors

--- strax/storage/mongo.py
+++ strax/storage/mongo.py
@@ -1,21 +1,11 @@
 """I/O format for MongoDB
 
-This plugin is designed for small amounts of basic information and 
-is specifically implemented for an online data monitoring system.
-Each chunk is processed separately and extracted information is stored
-in a single MongoDB document of the following form:
+This plugin is designed with data monitoring in mind, to put smaller
+amounds of extracted data into a database for quick access. However
+it should work with any plugin.
 
-doc = {
-  number: run_id,
-  plugin: plugin_name,
-  chunk: chunk_id,
-  data: {  PLUGIN_DATA },
-  }
-}
-
-Note that you must define Mongo connectivity parameters are environment
-variables. Note also that there is a 16 MB size limit on MongoDB documents,
-so plugins must be relatively light on data.
+Note that there is no check to make sure the 16MB document size
+limit is respected!
 """
 
 try:
@@ -32,6 +22,7 @@ import copy
 
 export, __all__ = strax.exporter()
 
+
 @export
 class MongoBackend(StorageBackend):
 
@@ -41,7 +32,7 @@ class MongoBackend(StorageBackend):
                               "cannot use MongoDBStore")
         self.client = MongoClient(uri)
         self.db = self.client[database]
-        
+
     def _read_chunk(self, backend_key, chunk_info, dtype, compressor):
 
         doc = self.db[backend_key].find_one(
@@ -54,27 +45,27 @@ class MongoBackend(StorageBackend):
             for key in np.dtype(dtype).names:
                 result[key][i] = d[key]
         return result
-            
+
     def _saver(self, key, metadata):
         return MongoSaver(key, metadata, self.db[str(key)])
 
     def get_metadata(self, key):
         return self.db[key].find_one({'metadata': True})
 
-    
+
 @export
 class MongoFrontend(StorageFrontend):
-    """MongoDB storage frontend. For high-level data"""
+    """MongoDB storage frontend"""
 
-    def __init__(self, uri, database, *args, **kwargs):        
-        super().__init__(*args, **kwargs)        
+    def __init__(self, uri, database, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         if MongoClient is None:
             raise ImportError("Pymongo did not import: "
                               "cannot use MongoDBStore")
         self.client = MongoClient(uri)
         self.db = self.client[database]
         self.backends = [MongoBackend(uri, database)]
-        
+
     def _find(self, key, write, allow_incomplete, fuzzy_for,
               fuzzy_for_options):
 
@@ -93,42 +84,42 @@ class MongoFrontend(StorageFrontend):
         return doc
 
 
-
 @export
 class MongoSaver(strax.Saver):
     prefer_rechunk = False
-    
+
     def __init__(self, key, metadata, col):
         super().__init__(metadata)
         self.col = col
         self.col.insert(dict(metadata=True, **self.md))
-        
-    def _save_chunk(self, data, chunk_info, executor):        
+
+    def _save_chunk(self, data, chunk_info, executor):
         chunk = chunk_info['chunk_i']
         for row in data:
             ins = {}
-
-            for key in list(data.dtype.names):                
+            for key in list(data.dtype.names):
                 ins[key] = row[key]
             ins = remove_np(ins)
             self.col.update({"chunk": chunk},
                             {"$push": {"data": ins}},
                             upsert=True)
         return dict(), None
-                
+
     def _save_chunk_metadata(self, chunk_info):
         self.col.find_one_and_update(dict(metadata=True),
                                      {'$push': {'chunks': chunk_info}})
-        
+
     def _close(self):
         update = {k: v
-                for k, v in self.md.items()
+                  for k, v in self.md.items()
                   if k in ('writing_ended', 'exception')}
         self.col.find_one_and_update(dict(metadata=True),
                                      {'$set': update})
 
-def remove_np(dictin):
 
+def remove_np(dictin):
+    """Remove numpy types from a dict so it can be inserted into
+    mongo."""
     if isinstance(dictin, dict):
         result = {}
         for k in dictin.keys():
