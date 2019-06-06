6a2cc6ce228320c172d2b7121f08e947e46f319b
JelleAalbers <jelleaalbers@gmail.com>
2019-06-06 08:21:31 -0500

    Improve empty chunk handling (see also #162)

--- tests/test_chunk_arrays.py
+++ tests/test_chunk_arrays.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import pytest
 from . import helpers   # Mocks numba    # noqa
@@ -20,8 +21,43 @@ def source_2():
     return f()
 
 
+class CorrectlyExhaustedEmptySource(Exception):
+    pass
+
+
+def _s_empties(crash=True):
+    items_yielded = 0
+    for i in itertools.count():
+        if i % 2:
+            yield np.ones(0, dtype=np.int64)
+        else:
+            yield np.ones(1, dtype=np.int64) * items_yielded
+            items_yielded += 1
+        if crash:
+            if items_yielded == 1000:
+                raise CorrectlyExhaustedEmptySource
+
+
+@pytest.fixture
+def source_some_empty_crasher():
+    # Source that yields a mix of empty and non-empty arrays
+    # Tests should check custom exception at end gets raised
+    return _s_empties(crash=True)
+
+
+@pytest.fixture
+def source_some_empty():
+    return _s_empties(crash=False)
+
+
 def test_get_next(source):
     p = strax.ChunkPacer(source)
+    
+    # Getting nothing results in empty array
+    r = p.get_n(0)
+    assert len(r) == 0
+    assert r.dtype == np.int64
+    
     result = []
     while True:
         try:
@@ -65,6 +101,13 @@ def test_get_until(source):
         p.get_until(5000)
 
 
+def test_get_some_emty(source_some_empty_crasher):
+    p = strax.ChunkPacer(source_some_empty_crasher)
+    p.get_n(1000)
+    with pytest.raises(CorrectlyExhaustedEmptySource):
+        p.get_n(1)
+
+
 def test_fixed_length_chunks(source):
     # TODO: test chunk size < array
     # test chunk size > array
@@ -96,6 +139,7 @@ def test_same_length(source, source_2):
 
 @pytest.fixture
 def source_skipper():
+    # Source that only returns even numbers from 0-1000 
     def f():
         for i in range(100):
             yield np.arange(0, 10, 2) + 10 * i
@@ -109,13 +153,36 @@ def test_same_stop(source, source_skipper):
                    [x[1] for x in result])
 
 
+def test_same_stop_some_empty(source_some_empty, source_skipper):
+    result = list(strax.same_stop(source_some_empty, source_skipper))
+
+    _do_sync_check([x[0] for x in result],
+                   [x[1] for x in result])
+    
+
 def _do_sync_check(r1, r2):
+    assert len(r1) == len(r2)
+    # Checks order and length are OK
     _check_mangling(r1)
     _check_mangling(r2, total_length=500, diff=2)
+    
+    seen_r1 = 0
+    seen_r2 = 0
     for i in range(len(r1)):
-        assert r2[i][-1] <= r1[i][-1]
-        if i != len(r1) - 1:
-            assert r2[i + 1][-1] > r1[i][-1]
+        # If pacemaker is empty, other r is also
+        if not len(r1[i]):
+            assert not len(r2[i])
+            continue
+            
+        # Second r does not lag behind first
+        if len(r2[i]):
+            assert min(r2[i]) >= seen_r1
+            seen_r2 = max(r2[i])
+            
+        seen_r1 = max(r1[i])
+        
+        # Second r does not outpace first
+        assert seen_r1 >= seen_r2
 
 
 def test_sync_iters(source, source_skipper):
