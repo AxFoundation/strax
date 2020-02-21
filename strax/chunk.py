FIx chunk splitting, off-by-one

strax/chunk.py
@@ -39,6 +39,7 @@ class Chunk:
         assert isinstance(data, np.ndarray)
         assert data.dtype == dtype
         dtype = np.dtype(dtype)
+        assert end >= start
 
         self.data_type = data_type
         self.data_kind = data_kind
@@ -68,16 +69,17 @@ class Chunk:
                 run_id=self.run_id),
             **kwargs)
 
-    def split(self, n_items: ty.Union[int, None], at: ty.Union[int, None]):
+    def split(self,
+              n_items : ty.Union[int, None] = None,
+              at: ty.Union[int, None] = None):
         if n_items is None and at is None:
             raise ValueError("Provide either n_items or at")
         if n_items is not None and at is not None:
             raise ValueError("Don't provide both n_items and at")
 
         if n_items is None:
-            n_items = strax.first_index_not_below(
-                strax.endtime(self.data),
-                at)
+            n_items = strax.first_true(
+                strax.endtime(self.data) > at)
 
         data1, data2 = np.split(self.data, [n_items])
 
@@ -97,11 +99,12 @@ class Chunk:
             data_kind=self.data_kind)
 
         c1 = strax.Chunk(start=self.start,
+                         # NB: this can extend the chunk!
                          end=at,
                          data=data1,
                          **common_kwargs)
         c2 = strax.Chunk(start=at,
-                         end=self.end,
+                         end=max(at, self.end),
                          data=data2,
                          **common_kwargs)
         return c1, c2

tests/test_chunk_arrays.py
@@ -15,7 +15,7 @@ _common_kwargs = frozendict(
 def _make_chunk(ts):
     data = np.zeros(len(ts), dtype=strax.interval_dtype)
     data['time'] = ts
-    data['length'] = 0
+    data['length'] = 1
     data['dt'] = 1
 
     return strax.Chunk(
@@ -111,10 +111,15 @@ def test_get_until(source):
     for t in thresholds:
         result.append(unchunk(p.get_until(t)))
 
+    # We're testing with length 1 intervals here,
+    # so end <= 457 is last satisfied by item that starts at 456
+    # which is the 457th item
     _check_mangling(result, total_length=457)
 
     result.append(unchunk(p.get_until(678)))
 
+    # Remember we have length 1 intervals, so
+    # start times must be < the threshold in the same block
     assert all([result[i][-1] < thresholds[i]
                 for i in range(len(thresholds))])
     assert all([result[i + 1][0] >= thresholds[i]
@@ -215,7 +220,7 @@ def _do_sync_check(r1, r2):
         seen_r1 = max(r1[i])
 
         # Second r does not outpace first
-        assert seen_r1 >= seen_r2
+        assert seen_r1 >= seen_r2, f"{i}, {r1[i]}, {r2[i]}"
 
 
 def test_sync_iters(source, source_skipper):