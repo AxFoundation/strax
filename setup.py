00059c8799f22d6427439843304426c7e52fc45a
Jelle Aalbers <jelleaalbers@gmail.com>
2020-03-15 12:34:39 +0100

    Bump version: 0.9.1 → 0.10.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='strax',
-                 version='0.9.1',
+                 version='0.10.0',
                  description='Streaming analysis for xenon TPCs',
                  author='Jelle Aalbers',
                  url='https://github.com/AxFoundation/strax',
