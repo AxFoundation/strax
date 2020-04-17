42d26177630a3bf43eb68df6f167067e9a0d3a6f
Daniel Wenz <dwenz@kabelmail.de>
2020-04-17 14:01:24 +0200

    Changed _index_of_fraction to be called per peak

--- strax/processing/peak_properties.py
+++ strax/processing/peak_properties.py
@@ -1,58 +1,59 @@
 import numpy as np
 import numba
 
+import strax
+export, __all__ = strax.exporter()
 __all__ = 'compute_widths'.split()
 
 
+@numba.njit(cache=True, nogil=True)
 def index_of_fraction(peaks, fractions_desired):
     # nopython does not allow this dynamic allocation:
-    results = np.zeros((len(peaks), len(fractions_desired)), dtype=np.float)
-    _index_of_fraction(peaks, fractions_desired, results)
-    return results
+    results = np.zeros((len(peaks), len(fractions_desired)), dtype=np.float32)
 
-
-@numba.jit(nopython=True, nogil=True, cache=True)
-def _index_of_fraction(peaks, fractions_desired, results):
     for p_i, p in enumerate(peaks):
         area_tot = p['area']
         if area_tot <= 0:
             continue  # TODO: These occur a lot. Investigate!
         r = results[p_i]
+        compute_index_of_fraction(p, area_tot, fractions_desired, r)
+    return results
 
-        fraction_seen = 0
-        current_fraction_index = 0
-        needed_fraction = fractions_desired[current_fraction_index]
-        for i, x in enumerate(p['data'][:p['length']]):
-            # How much of the area is in this sample?
-            fraction_this_sample = x / area_tot
+@export
+@numba.jit(nopython=True, nogil=True, cache=True)
+def compute_index_of_fraction(peak, area_tot, fractions_desired, result):
+    fraction_seen = 0
+    current_fraction_index = 0
+    needed_fraction = fractions_desired[current_fraction_index]
+    for i, x in enumerate(peak['data'][:peak['length']]):
+        # How much of the area is in this sample?
+        fraction_this_sample = x / area_tot
 
-            # Are we passing any desired fractions in this sample?
-            while fraction_seen + fraction_this_sample >= needed_fraction:
+        # Are we passing any desired fractions in this sample?
+        while fraction_seen + fraction_this_sample >= needed_fraction:
 
-                area_needed = area_tot * (needed_fraction - fraction_seen)
-                if x != 0:
-                    r[current_fraction_index] = i + area_needed / x
-                else:
-                    r[current_fraction_index] = i
+            area_needed = area_tot * (needed_fraction - fraction_seen)
+            if x != 0:
+                result[current_fraction_index] = i + area_needed / x
+            else:
+                result[current_fraction_index] = i
 
-                # Advance to the next fraction
-                current_fraction_index += 1
-                if current_fraction_index > len(fractions_desired) - 1:
-                    break
-                needed_fraction = fractions_desired[current_fraction_index]
-
+            # Advance to the next fraction
+            current_fraction_index += 1
             if current_fraction_index > len(fractions_desired) - 1:
                 break
+            needed_fraction = fractions_desired[current_fraction_index]
 
-            # Add this sample's area to the area seen
-            fraction_seen += fraction_this_sample
+        if current_fraction_index > len(fractions_desired) - 1:
+            break
 
-        if needed_fraction == 1:
-            # Sometimes floating-point errors prevent the full area
-            # from being reached before the waveform ends
-            r[-1] = p['length']
+        # Add this sample's area to the area seen
+        fraction_seen += fraction_this_sample
 
-    return results
+    if needed_fraction == 1:
+        # Sometimes floating-point errors prevent the full area
+        # from being reached before the waveform ends
+        result[-1] = peak['length']
 
 
 def compute_widths(peaks):
