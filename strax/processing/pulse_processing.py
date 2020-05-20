d0759f55f97c210651b9124eb55c187e146393d2
Sophia Andaloro <sja5@rice.edu>
2020-05-20 16:46:13 -0500

    fix flip boolean bug
    using np.where with flip as boolean, will return -1 if flip=True, else returns 1. 
    Previously boolean meant if flip=False d['data'] will go to 0.

--- strax/processing/pulse_processing.py
+++ strax/processing/pulse_processing.py
@@ -66,7 +66,7 @@ def baseline(records, baseline_samples=40, flip=True,
         # Subtract baseline from all data samples in the record
         # (any additional zeros should be kept at zero)
         d['data'][:d['length']] = (
-            (-1 * flip) * (d['data'][:d['length']] - int(bl)))
+            np.where(flip, -1, 1) * (d['data'][:d['length']] - int(bl)))
         d['baseline'] = bl
         d['baseline_rms'] = rms
 
