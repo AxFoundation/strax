ba6e1e0d3fbc2feef2445ab955dbe1d8150fb3e8
Joran Angevaare <jorana@nikhef.nl>
2020-07-21 09:26:50 +0200

    Correctly check endtimes in chunk.py (#281)
    We observed that although the data is always sorted by (start)time, this is not per se true for the endtime. Therefore the last record may not have the latest endtime. This is the safest way to get around this.
    
    Depending on the datarate one might also argue that for example we look at the last 500 samples :
    ```
    data_ends_at = strax.endtime(self.data[-500:]).max()
    ```
    * Check last 500 samples for data_ends_at

--- strax/chunk.py
+++ strax/chunk.py
@@ -65,7 +65,8 @@ class Chunk:
 
         if len(self.data):
             data_starts_at = self.data[0]['time']
-            data_ends_at = strax.endtime(self.data[-1])
+            # Check the last 500 samples (arbitrary number) as sanity check
+            data_ends_at = strax.endtime(self.data[-500:]).max()
 
             if data_starts_at < self.start:
                 raise ValueError(f"Attempt to create chunk {self} "
