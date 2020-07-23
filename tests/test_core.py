e1d7878e5054966568058350416172111eca769e
Joran Angevaare <jorana@nikhef.nl>
2020-07-23 16:38:37 +0200

    add test for progress_bar
    This test would have caught #296

--- tests/test_core.py
+++ tests/test_core.py
@@ -253,9 +253,11 @@ def test_random_access():
             st.get_array(run_id, 'peaks')
 
         df = st.get_array(run_id, 'peaks', time_range=(3, 5))
+        df_pbar = st.get_array(run_id, 'peaks', time_range=(3, 5), progress_bar = False)
         assert len(df) == 2 * recs_per_chunk
         assert df['time'].min() == 3
         assert df['time'].max() == 4
+        assert np.all(df == df_pbar)
 
         # Try again with unaligned chunks
         df = st.get_array(run_id, ['peaks', 'peak_classification'],
