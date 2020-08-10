4003462bd42f1cf12048d8e0df11052bbab933e7
ahiguera-mx <ahiguera-mx@fried.rice.edu>
2020-08-10 18:39:05 -0500

    add message when KeyError found for better debugging

--- strax/corrections/cmt_services.py
+++ strax/corrections/cmt_services.py
@@ -15,13 +15,16 @@ def get_elife(run_id, global_version = 'v1', xenon1t=False):
     else:
         df_global = strax.read('global')
         my_correction = 'elife'
-   
-    for correction, version in df_global.iloc[-1][global_version].items():
-        if my_correction == correction:
-            df = strax.read(correction)
-            df = strax.interpolate(df, when)
-    
-    return df.loc[df.index == when, version].values[0]
+  
+    try:
+        for correction, version in df_global.iloc[-1][global_version].items():
+            if my_correction == correction:
+                df = strax.read(correction)
+                df = strax.interpolate(df, when)
+    except KeyError:
+            raise ValueError(f'Global version {global_version} not found')
+
+    return df.loc[df.index == when, global_version].values[0]
 
 @export
 def get_pmt_gains(run_id, global_version = 'v1', xenon1t=False):
@@ -35,11 +38,16 @@ def get_pmt_gains(run_id, global_version = 'v1', xenon1t=False):
     
     # equivalent to 'to_pe' in gains_model
     gains =[] 
-    for correction, version in df_global.iloc[-1][global_version].items():
-        if 'pmt' in correction:
-            df = strax.read(correction)
-            df = strax.interpolate(df, when)
-            gains.append(df.loc[df.index == when, version].values[0])
+    try:
+        for correction, version in df_global.iloc[-1][global_version].items():
+            if 'pmt' in correction:
+                df = strax.read(correction)
+                df = strax.interpolate(df, when)
+                gains.append(df.loc[df.index == when, version].values[0])
             
-        pmt_gains=np.asarray(gains,dtype=np.float32)
+            pmt_gains=np.asarray(gains,dtype=np.float32)
+
+    except KeyError:
+        raise ValueError(f'Global version {global_version} not found')
+
     return pmt_gains
