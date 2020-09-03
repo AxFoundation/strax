385389488bb29b4bb9f4d9755b1a3025c91e1bd0
Darryl Masson <darrylmasson@gmail.com>
2020-09-03 10:47:50 +0200

    Throws instead of figuring it out

--- strax/plugin.py
+++ strax/plugin.py
@@ -608,9 +608,9 @@ class LoopPlugin(Plugin):
             loop_over = self.loop_over
         else:
             loop_over = self.deps[self.depends_on[0]].data_kind
-            if isinstance(loop_over, dict):
-                loop_over = loop_over[self.depends_on[0]]
-                # We can't have nested dictionaries here, can we?
+            if not isinstance(loop_over, str):
+                raise TypeError("Please add \"loop_over = <base>\""
+                                " to your plugin definition")
 
         # Group into lists of things (e.g. peaks)
         # contained in the base things (e.g. events)
