6cc321b4fb608ca30992b46989d58e6005abe6cd
ahiguera-mx <ahiguera-mx@fried.rice.edu>
2020-09-09 22:20:52 -0500

    Add useful info to docstring, require that DatetimeIndex are in UTC and checking that

--- strax/corrections.py
+++ strax/corrections.py
@@ -1,14 +1,16 @@
-'''I/O format for corrections using MongoDB
+"""I/O format for corrections using MongoDB
 
 This module list, reads, writes, among others functionalities
 to a MongoDB where corrections are stored.
-'''
+"""
 
 from datetime import datetime
+from datetime import timezone
 import logging
 import pandas as pd
 import pdmongo as pdm
 import pymongo
+import pytz
 
 import strax
 
@@ -17,16 +19,23 @@ export, __all__ = strax.exporter()
 
 @export
 class CorrectionsInterface:
-    '''A class to manage corrections that are stored in a MongoDB
+    """
+    A class to manage corrections that are stored in a MongoDB,
     corrections are defined as pandas.DataFrame with a
-    pandas.DatetimeIndex, an v1 and online version must be specified,
-    online versions are meant for online processing, whereas v1 and
-    so on are meant for offline processing. A Global configuration
-    is available, meaning a unique set of correction maps.
-    '''
+    pandas.DatetimeIndex in UTC, a v1 and online version must be specified,
+    online versions are meant for online processing, whereas v1, v2, v3...
+    are meant for offline processing. A Global configuration can be set,
+    this means a unique set of correction maps.
+    """
 
-    def __init__(self, host='127.0 0.1', username=None, password=None,
+    def __init__(self, host='127.0.0.1', username=None, password=None,
                  database_name='corrections'):
+        """
+        :param host: DB host
+        :param username: DB username
+        :param password: DB password
+        :param database_name: DB name
+        """
 
         self.host = host
         self.username = username
@@ -38,14 +47,14 @@ class CorrectionsInterface:
         self.database = self.client[self.database_name]
 
     def list_corrections(self):
-        '''Smart logic to list all corrections.
-        '''
+        """Smart logic to list all corrections.
+        """
         return [x['name'] for x in self.database.list_collections()]
 
     def read(self, correction):
-        '''Smart logic to read corrections,
-        :param correction: correction's name (str type).
-        '''
+        """Smart logic to read corrections,
+        :param correction: pandas.DataFrame object name in the DB (str type).
+        """
         df = pdm.read_mongo(correction, [], self.database)
 
         # No data found
@@ -53,16 +62,23 @@ class CorrectionsInterface:
             return None
         # Delete internal Mongo identifier
         del df['_id']
+
+        df['time'] = pd.to_datetime(df['time'], utc=True)
+
         return df.set_index('time')
 
-    def interpolate(self, what, when, how='interpolate', **kwargs):
-        '''Interpolate values of a given correction
+    @staticmethod
+    def interpolate(what, when, how='interpolate', **kwargs):
+        """Interpolate values of a given correction
         For information of interpolation methods see,
         https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.interpolate.html
         :param what: what do you want to interpolate, what correction(DataFrame)
-        :param when: date,  e.g. datetime(2020, 8, 12, 21, 4, 32, 726110)
-        :param how: Interpolation method
-        '''
+        :param when: date, e.g. datetime(2020, 8, 12, 21, 4, 32, 7, tzinfo=pytz.utc)
+        :param how: Interpolation method, can be either how='interpolate' or how='fill'
+        :param kwargs: are forward to the interpolation
+        """
+        CorrectionsInterface.check_timezone(when)
+
         df = what
 
         df_new = pd.DataFrame.from_dict({'Time': [when]})
@@ -75,7 +91,7 @@ class CorrectionsInterface:
         if how == 'interpolate':
             df_combined = df_combined.interpolate(**kwargs)  # method='linear' is the default
         elif how == 'fill':
-            df_combined = df_combined.ffill(**kwards)
+            df_combined = df_combined.ffill(**kwargs)
         else:
             raise ValueError('Specify an interpolation method, e.g. interpolate or fill')
 
@@ -83,39 +99,40 @@ class CorrectionsInterface:
 
     def get_context_config(self, when, global_config='global',
                            global_version='v1'):
-        '''Global configuration logic
-        :param when: date e.g. datetime(2020, 8, 12, 21, 4, 32, 726110)
+        """Global configuration logic
+        :param when: date e.g. datetime(2020, 8, 12, 21, 4, 32, 7, tzinfo=pytz.utc)
         :param global_config: a map of corrections
         :param global_version: global configuration's version
-        '''
+        """
+        CorrectionsInterface.check_timezone(when)
         df_global = self.read(global_config)
 
         context_config = {}
         # loop over corrections and versions to get a global context
         for correction, version in df_global.iloc[-1][global_version].items():
             df = self.read(correction)
-            df = self.interpolate(df, when)
+            df = CorrectionsInterface.interpolate(df, when)
             context_config[correction] = df.loc[df.index == when,
                                                 version].values[0]
 
         return context_config
 
     def write(self, correction, df, required_columns=('ONLINE', 'v1')):
-        '''Smart logic to write corrections
-        :param correction: corrections' name (str type)
-        :param df: pandas DataFrame.
-        :required_columns: DataFrame must include a online and v1 columns
-        '''
+        """Smart logic to write corrections
+        :param correction: corrections is a pandas.DataFrame object, corrections name (str type)
+        :param df: corrections is a pandas.DataFrame object a DatetimeIndex
+        :required_columns: DataFrame must include an online and v1 columns
+        """
         for req in required_columns:
             if req not in df.columns:
-                raise ValueError(f'Muts specify {req} in dataframe')
+                raise ValueError(f'Must specify {req} in dataframe')
         # Compare against
         logging.info('Reading old values for comparison')
         df2 = self.read(correction)
 
         if df2 is not None:
             logging.info('Checking if columns unchanged in past')
-            now = datetime.utcnow()
+            now = datetime.now(tz=timezone.utc)
             for column in df2.columns:
                 logging.debug(f'Checking {column}')
                 if not (df2.loc[df2.index < now, column] ==
@@ -126,3 +143,12 @@ class CorrectionsInterface:
         logging.info('Writing')
 
         return df.to_mongo(correction, self.database, if_exists='replace')
+
+    def check_timezone(date):
+        """Smart logic to check date is given in UTC time zone
+        :param date: date e.g. datetime(2020, 8, 12, 21, 4, 32, 7, tzinfo=pytz.utc)
+        """
+        if date.tzinfo == pytz.utc:
+            return date
+        else:
+            raise ValueError(f'{date} must be specify in UTC timezone')
