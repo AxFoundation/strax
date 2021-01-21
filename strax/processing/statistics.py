8017f6725dfeacea38bac6803e12b708bb921f33
Daniel Wenz <dwenz@kabelmail.de>
2021-01-21 08:29:41 +0100

    Added comments and renamed variables

--- strax/processing/statistics.py
+++ strax/processing/statistics.py
@@ -36,65 +36,80 @@ def highest_density_region(data, fractions_desired, _buffer_size=10):
 
     area_tot = np.sum(data)
 
-    # Need index sorted by amplitude
+    # Need an index which sorted by amplitude
     max_to_min = np.argsort(data)[::-1]
 
-    lowest_point = np.inf
+    lowest_sample_seen = np.inf
     for j in range(1, len(data) - 1):
-        # Computing fractions from max to min
-        if lowest_point == data[max_to_min[j]]:
+        # Loop over indices compute fractions from max to min
+        if lowest_sample_seen == data[max_to_min[j]]:
             # We saw this sample height already, so no need to repeat
             continue
 
-        lowest_point = data[max_to_min[j]]
-        d = data[max_to_min[:j]]
-        f = np.sum(d - lowest_point) / area_tot
+        lowest_sample_seen = data[max_to_min[j]]
+        sorted_data_max_to_j = data[max_to_min[:j]]
+        fraction_seen = np.sum(sorted_data_max_to_j - lowest_sample_seen) / area_tot
 
         # Check if this height step exceeded at least one of the desired
         # fractions
-        m = fractions_desired[fi:] <= f
-        if np.any(m):
-            for fd in fractions_desired[fi:fi + np.sum(m)]:
-                # Compute correct height for which we reach desired
-                # fraction and store:
-                g = fd / f
-                h = (1 - g) * np.sum(d) / j + g * lowest_point
-                res_amp[fi] = h
+        m = fractions_desired[fi:] <= fraction_seen
+        if not np.any(m):
+            # If we do not exceed go to the next sample.
+            continue
 
-                # Find gaps and get edges of hdr intervals:
-                ind = np.sort(max_to_min[:j])
-                gaps = np.arange(1, len(ind) + 1)
+        for fraction_desired in fractions_desired[fi:fi + np.sum(m)]:
+            # Since we loop always to the height of the next highest sample
+            # it might happen that we overshoot the desired fraction. Similar
+            # to the area deciles algorithm we have now to figure out at which
+            # height we actually reached the desired fraction and store the
+            # corresponding height:
+            g = fraction_desired / fraction_seen
 
-                g0 = 0
-                g_ind = -1
-                diff = ind[1:] - ind[:-1]
-                gaps = gaps[:-1][diff > 1]
-                if len(gaps):
-                    mes = ('Found more intervals than "_buffer_size" allows please'
-                           'increase the size of the buffer.')
-                    assert len(gaps) < _buffer_size, mes
+            # The following gives the true height, to get here one has to
+            # solve for h:
+            # 1. fraction_seen = sum_{i=0}^j (y_i - y_j) / a_total
+            # 2. fraction_desired = sum_{i=0}^j (y_i - h) / a_total
+            # 3. g = fraction_desired/fraction_seen
+            # j == number of seen samples
+            # n == number of total samples in distribution
+            true_height = (1 - g) * np.sum(sorted_data_max_to_j) / j + g * lowest_sample_seen
+            res_amp[fi] = true_height
 
-                    for g_ind, g in enumerate(gaps):
-                        # Loop over all gaps and get outer edges:
-                        interval = ind[g0:g]
-                        res[fi, 0, g_ind] = interval[0]
-                        res[fi, 1, g_ind] = interval[-1] + 1
-                        g0 = g
+            # Find gaps and get edges of hdr intervals:
+            ind = np.sort(max_to_min[:j])
+            gaps = np.arange(1, len(ind) + 1)
 
-                # Now we have to do the last interval:
-                interval = ind[g0:]
-                res[fi, 0, g_ind + 1] = interval[0]
-                res[fi, 1, g_ind + 1] = interval[-1] + 1
+            g0 = 0
+            g_ind = -1
+            diff = ind[1:] - ind[:-1]
+            gaps = gaps[:-1][diff > 1]
+            if len(gaps):
+                mes = ('Found more intervals than "_buffer_size" allows please'
+                       'increase the size of the buffer.')
+                assert len(gaps) < _buffer_size, mes
 
-                fi += 1
+                for g_ind, g in enumerate(gaps):
+                    # Loop over all gaps and get outer edges:
+                    interval = ind[g0:g]
+                    res[fi, 0, g_ind] = interval[0]
+                    res[fi, 1, g_ind] = interval[-1] + 1
+                    g0 = g
+
+            # Now we have to do the last interval:
+            interval = ind[g0:]
+            res[fi, 0, g_ind + 1] = interval[0]
+            res[fi, 1, g_ind + 1] = interval[-1] + 1
+
+            fi += 1
 
         if fi == (len(fractions_desired)):
+            # Found all fractions so we are done
             return res, res_amp
     
     # If we end up here this might be due to an offset 
-    # of the distribution with respect to zero. In that case
-    # there is only a single interval which has the the size 
-    # of the entire distribution. 
+    # of the distribution with respect to zero. In that case it can
+    # happen that we do not find all desired fractions.
+    # The remaining intervals have a size of the entire distribution.
     res[fi:, 0, 0] = 0
     res[fi:, 0, 1] = len(data)+1
     res_amp[fi:] = 0
