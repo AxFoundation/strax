7524b75df78b18b43639634d34eaaba550048dd6
Peter Gaemers <petergaemers@hotmail.com>
2021-02-05 15:55:03 +0100

    Update peak_building.py

--- strax/processing/peak_building.py
+++ strax/processing/peak_building.py
@@ -104,7 +104,7 @@ def find_peaks(hits, adc_to_pe,
 
 
 @export
-@numba.jit(nopython=True, nogil=True, cache=False)
+@numba.jit(nopython=True, nogil=True, cache=True)
 def store_downsampled_waveform(p, wv_buffer, wv_top_buffer, wv_bottom_buffer):
     """Downsample the waveform in buffer and store it in p['data']
 
