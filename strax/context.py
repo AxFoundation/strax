ac3c769c61b7e11d4426081c56d51a6301ced7dd
Joran Angevaare <jorana@nikhef.nl>
2021-03-12 14:51:03 +0100

    only the first target is used

--- strax/context.py
+++ strax/context.py
@@ -771,7 +771,7 @@ class Context:
         if len(intersec):
             raise RuntimeError(f"{intersec} both computed and loaded?!")
         if len(targets) > 1:
-            final_plugin = self._get_end_targets(plugins)
+            final_plugin = self._get_end_targets(plugins)[:1]
             self.log.warning(
                 f'Multiple targets detected! This is only suitable for mass '
                 f'producing dataypes since only {final_plugin} will be '
