f63bf97e4c973d237052dc00646ee51536f4075c
Daniel Wenz <dwenz@kabelmail.de>
2021-10-12 09:07:39 +0200

    Propagate information peaks merging

--- strax/processing/peak_merging.py
+++ strax/processing/peak_merging.py
@@ -71,8 +71,9 @@ def merge_peaks(peaks, start_merge_at, end_merge_at,
         new_p['n_saturated_channels'] = new_p['saturated_channel'].sum()
 
         # Use the tight coincidence of the peak with the highest amplitude
-        new_p['tight_coincidence'] = old_peaks['tight_coincidence'][
-            old_peaks['data'].max(axis=1).argmax()]
+        index_largest_sub_peak = old_peaks['data'].max(axis=1).argmax()
+        new_p['tight_coincidence'] = old_peaks['tight_coincidence'][index_largest_sub_peak]
+        new_p['tight_coincidence_channel'] = old_peaks['tight_coincidence_channel'][index_largest_sub_peak]
         
         # If the endtime was in the peaks we have to recompute it here 
         # because otherwise it will stay set to zero due to the buffer
