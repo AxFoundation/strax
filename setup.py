c36d5a449ec40be16078a754c2e986f34dd05650
Joran Angevaare <jorana@nikhef.nl>
2022-01-24 09:21:52 +0100

    deprecate py3.6 py3.7

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ setuptools.setup(name='strax',
                  install_requires=requires,
                  tests_require=requires + tests_requires,
                  long_description=readme + '\n\n' + history,
-                 # We don't test 3.6 anymore, see #503
-                 python_requires=">=3.6",
+                 python_requires=">=3.8",
                  extras_require={
                      'docs': docs_requires
                  },
@@ -43,8 +42,6 @@ setuptools.setup(name='strax',
                      'Development Status :: 5 - Production/Stable',
                      'License :: OSI Approved :: BSD License',
                      'Natural Language :: English',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.9',
                      'Programming Language :: Python :: 3.10',
