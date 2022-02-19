e6d728d3494d26ae05a6830ee648d5fa879ee686
Joran Angevaare <jorana@nikhef.nl>
2022-02-19 20:03:51 +0100

    Add buffer plugin

--- strax/plugin.py
+++ strax/plugin.py
@@ -14,6 +14,7 @@ from immutabledict import immutabledict
 import numpy as np
 from copy import copy, deepcopy
 import strax
+import numba
 export, __all__ = strax.exporter()
 
 
@@ -623,6 +624,131 @@ class Plugin:
 
     def compute(self, **kwargs):
         raise NotImplementedError
+
+
+class RechunkerPlugin(Plugin):
+    """
+    This is a rather brute force plugin. It keeps a state and as such does
+    not allow for parallization and other nice strax features (similar to
+    the OverLapWindowPlugin). It's intended to be used for data that is sparse
+    in times and is hard to fit into the chunking paradigm.
+    Examples are:
+     - handing logic signals, that can span minutes and are NOT sorted in
+       endtime (assumed by OverlapWindowPlugin), and requite a lot of overhead
+       if all chunks were buffered in an OverlapWindowPlugin.
+     - Simulation plugins where one might realize that there was actually a
+
+    Works similar to the OverLapWindowPlugin.
+
+    Required class functions:
+      - send_input_after, should
+      - min_gap_in_chunk
+    """
+    parallel = False
+    max_buffer_mb = 1_000
+    _NO_SPLIT_FOUND = -42
+
+    def send_input_after(self)->int:
+        """After how long buffering can we start sending the output"""
+        raise NotImplementedError
+
+    def min_gap_in_chunk(self)->int:
+        """How long does the gap need to be minimally to the next chunk"""
+        raise NotImplementedError
+
+    def __init__(self)->None:
+        super().__init__()
+        self.cached_input = {}
+
+    def iter(self, iters, executor=None)->strax.Chunk:
+        yield from super().iter(iters, executor=executor)
+
+        if self.cached_input:
+            final_chunk = self.get_final_result()
+            yield final_chunk
+
+    def _input_is_old_enough(self) -> bool:
+        """
+        Did we wait long enough to try start sending data,
+        check is not run on the last bit of data on the final iter
+        """
+        for data_type, new_input in self.cached_input.items():
+            t0 = self.cached_input.get(data_type, new_input).start
+            t1 = new_input.end
+            # We need to have to start sending results
+            if t1-t0 < self.send_input_after() + self.min_gap_in_chunk():
+                return False
+        return True
+
+    def _input_to_cache(self, kwargs)->None:
+        """Push new input to the cache and check this is not getting out of hand"""
+        for data_kind, chunk in kwargs.items():
+            if data_kind in self.cached_input:
+                self.cached_input[data_kind] = strax.Chunk.concatenate(
+                    [self.cached_input[data_kind], chunk])
+            else:
+                self.cached_input[data_kind] = chunk
+        tot_size = sum([c.nbytes for c in self.cached_input.items()])
+        if tot_size >= self.max_buffer_mb:
+            # If you are lazy, just increase the self.max_buffer_mb
+            raise ValueError(f'Buffer size of {self} exceeds the '
+                             f'{self.max_buffer_mb} MB limit, your data is '
+                             f'either too big or too dense for the '
+                             f'{self.min_gap_in_chunk()} ns gap or data')
+
+    # Can be jitted and/or optimized
+    def _find_split_in_input(self):
+        """
+        Find the latest time where the gap to the next start time is
+        larger than the requested gap
+        """
+        if any(not len(c) for c in self.cached_input.values()):
+            return self._NO_SPLIT_FOUND
+        all_starts = np.concatenate([c.data['time'] for c in self.cached_input.values()])
+        all_ends = np.concatenate([strax.endtime(c.data) for c in self.cached_input.values()])
+        old_enough = all_ends < all_ends[-1]-self.send_input_after()
+        if len(all_starts) <= 1:
+            # No reason to split this stuff
+            return self._NO_SPLIT_FOUND
+        sort = np.argsort(all_starts[old_enough])
+        starts = all_starts[old_enough][sort]
+        ends = all_ends[old_enough][sort]
+        del all_ends, all_starts, old_enough
+        gap = starts[1:]-ends[:-1]
+        dt_suffices = gap > self.min_gap_in_chunk()
+        if np.any(dt_suffices):
+            last_stop = ends[-1:][dt_suffices[-1]]
+            return last_stop
+        return self._NO_SPLIT_FOUND
+
+    def get_final_result(self):
+        """On the last chunk, just compute whatever we have left and
+        clear the cache
+        """
+        result = super().do_compute(**self.cached_input)
+        del self.cached_input
+        return result
+
+    def do_compute(self, chunk_i=None, **kwargs):
+        self._input_to_cache(kwargs)
+        if not self._input_is_old_enough():
+            return self.empty_result()
+
+        # Should be plenty space after this point, but let's assume
+        # someone (or me) can make an off by one error
+        last_split_t = self._find_split_in_input() + 1
+        if last_split_t == self._NO_SPLIT_FOUND:
+            return self.empty_result()
+        chunks_for_compute = {}
+        for data_type, chunk in self.cached_input.items():
+            early_chunk_bit, late_chunk_bit = chunk.split(t=last_split_t,
+                                                          allow_early_split=False)
+            chunks_for_compute[data_type] = early_chunk_bit
+            self.cached_input[data_type] = late_chunk_bit
+
+        # Compute new results
+        result = super().do_compute(chunk_i=chunk_i, **kwargs)
+        return result
 
 
 ##
