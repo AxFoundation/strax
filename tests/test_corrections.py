60b7d3396bf1e06a65e2c60e8de94b6e92bd6550
Joran Angevaare <j.angevaare@nikhef.nl>
2022-09-01 14:51:42 +0200

    maybe enough refactor?

--- tests/test_corrections.py
+++ tests/test_corrections.py
@@ -1,12 +1,10 @@
 import strax
 import pandas as pd
-import mongomock
 import numpy as np
 from datetime import datetime
 from pandas._testing import assert_frame_equal
 import pytz
 import pymongo
-import pytest
 import unittest
 from .test_mongo_frontend import _can_test
 import os
@@ -29,17 +27,12 @@ class TestCMT(unittest.TestCase):
     def setUp(self):
         uri = os.environ.get('TEST_MONGO_URI')
         db_name = 'test_mongosf_database_cmt'
+        database_name = 'corrections'
         client = pymongo.MongoClient(uri)
+        self.collection = client[db_name][database_name]
+        self.cmt = strax.CorrectionsInterface(client=client, database_name=db_name)
 
-
-    def corrections(self):
-        dummy_client = pymongo.MongoClient()
-        cmt = strax.CorrectionsInterface(client=dummy_client)
-        return cmt
-
-    def tearDown(self) -> None:
-        self.collection.drop()
-
+    @staticmethod
     def make_dummy_df():
         """
         Make a dummy pandas.dataframe()
@@ -55,9 +48,9 @@ class TestCMT(unittest.TestCase):
 
         return df
 
-    def test_db():
-        cmt = corrections()
-        df = make_dummy_df()
+    def test_db(self):
+        cmt = self.cmt()
+        df = self.make_dummy_df()
         # write to the DB
         cmt.write('test_db', df)
         # read from the DB
@@ -65,31 +58,31 @@ class TestCMT(unittest.TestCase):
         # pandas.DataFrame should be identical
         assert_frame_equal(df, df2)
 
-    def test_change_future():
+    def test_change_future(self):
         # add a new value in the future
-        cmt = corrections()
-        df = make_dummy_df()
+        cmt = self.cmt
+        df = self.make_dummy_df()
         cmt.write('test_change_future', df)
         df2 = cmt.read('test_change_future')
         df2.loc[pd.to_datetime(datetime(2029, 12, 1, 0, 0, 0, 0, tzinfo=pytz.utc))] = [15.0, 13.0, np.nan]
         df2 = df2.sort_index()
         cmt.write('test_change_future', df2)
 
-    def test_modify_nan():
+    def test_modify_nan(self):
         # modify non-physical values (NaN)
-        cmt = corrections()
-        df = make_dummy_df()
+        cmt = self.cmt
+        df = self.make_dummy_df()
         cmt.write('test_modify_nan', df)
         df2 = cmt.read('test_modify_nan')
         df2.loc[pd.to_datetime(datetime(2021, 9, 23, 0, 0, 0, 0, tzinfo=pytz.utc))] = [8.0, 14.0, 14.3]
         df2 = df2.sort_index()
         cmt.write('test_modify_nan', df2)
 
-    def test_change_past():
+    def test_change_past(self):
         # modify things in the past
         # fail unless raises a ValueError
-        cmt = corrections()
-        df = make_dummy_df()
+        cmt = self.cmt
+        df = self.make_dummy_df()
         cmt.write('test_change_past', df)
         df2 = cmt.read('test_change_past')
         df2.loc[pd.to_datetime(datetime(2021, 1, 1, 0, 0, 0, 0, tzinfo=pytz.utc))] = [7.0, 24.0, 14.3]
@@ -97,20 +90,20 @@ class TestCMT(unittest.TestCase):
         with unittest.TestCase().assertRaises(ValueError):
              cmt.write('test_change_past', df2)
 
-    def test_add_row():
+    def test_add_row(self):
         # add a new row for a new past date, allowed example
-        cmt = corrections()
-        df = make_dummy_df()
+        cmt = self.cmt
+        df = self.make_dummy_df()
         cmt.write('test_add_row', df)
         df2 = cmt.read('test_add_row')
         df2.loc[pd.to_datetime(datetime(2021, 2, 1, 0, 0, 0, 0, tzinfo=pytz.utc))] = [10.0, np.nan, 14.3]
         df2 = df2.sort_index()
         cmt.write('test_add_row', df2)
 
-    def test_add_row_2():
+    def test_add_row_2(self):
         # add a new row for a new past date, not allowed example
-        cmt = corrections()
-        df = make_dummy_df()
+        cmt = self.cmt
+        df = self.make_dummy_df()
         cmt.write('test_add_row_2', df)
         df2 = cmt.read('test_add_row_2')
         df2.loc[pd.to_datetime(datetime(2021, 2, 1, 0, 0, 0, 0, tzinfo=pytz.utc))] = [7.0, 24.0, 14.3]
