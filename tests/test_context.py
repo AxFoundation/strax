4941b066fcd007a63661ab5e6d8b593f8abc3c55
WenzDaniel <dwenz@kabelmail.de>
2023-11-22 11:12:06 -0600

    Remove tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -4,8 +4,6 @@ from strax.testutils import (
     Peaks,
     PeaksWoPerRunDefault,
     PeakClassification,
-    RecordsWithTimeStructure,
-    DownSampleRecords,
     run_id,
 )
 import tempfile
@@ -219,36 +217,6 @@ class TestContext(unittest.TestCase):
     def tearDown(self):
         if os.path.exists(self.tempdir):
             shutil.rmtree(self.tempdir)
-
-    def test_down_chunking(self):
-        st = self.get_context(False)
-        st.register(RecordsWithTimeStructure)
-        st.register(DownSampleRecords)
-
-        st.make(run_id, "records")
-        st.make(run_id, "records_down_chunked")
-
-        chunks_records = st.get_meta(run_id, "records")["chunks"]
-        chunks_records_down_chunked = st.get_meta(run_id, "records_down_chunked")["chunks"]
-
-        _chunks_are_downsampled = len(chunks_records) * 2 == len(chunks_records_down_chunked)
-        assert _chunks_are_downsampled
-
-        _chunks_are_continues = np.all([
-            chunks_records_down_chunked[i]["end"] == chunks_records_down_chunked[i + 1]["start"]
-            for i in range(len(chunks_records_down_chunked) - 1)
-        ])
-        assert _chunks_are_continues
-
-    def test_down_chunking_multi_processing(self):
-        st = self.get_context(False, allow_multiprocess=True)
-        st.set_context_config({"use_per_run_defaults": False})
-        st.register(RecordsWithTimeStructure)
-        st.register(DownSampleRecords)
-
-        st.make(run_id, "records", max_workers=1)
-        with self.assertRaises(NotImplementedError):
-            st.make(run_id, "records_down_chunked", max_workers=2)
 
     def test_get_plugins_with_cache(self):
         st = self.get_context(False)
