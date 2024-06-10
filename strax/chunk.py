d0cbc1b88425dcd28145fc7231ccf81f2d536847
Jelle Aalbers <JelleAalbers@users.noreply.github.com>
2024-06-10 16:49:57 +0200

    Single threaded alternative processor (#773)
    * add processors field to context
    
    * Separate rechunk logic from saver
    
    * Minor abstract processor changes/fixes
    
    * Single-thread processor + tests
    
    * Track time spent per data type
    
    * Fix final message lingering
    
    * Multi-output and rebase fixes, add logging
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Placate our code review robot overlords
    
    * Keep old straxen test running
    
    * Be compatible with hyperruns and fix code style
    
    * Code style fix
    
    ---------
    
    Co-authored-by: Yossi Mosbacher <joe.mosbacher>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dacheng Xu <dx2227@columbia.edu>

--- strax/chunk.py
+++ strax/chunk.py
@@ -420,3 +420,44 @@ def _update_subruns_in_chunk(chunks):
             else:
                 subruns[subrun_id] = subrun_start_end
     return subruns
+
+
+@export
+class Rechunker:
+    """Helper class for rechunking.
+
+    Send in chunks via receive, which returns either None (no chunk to send) or a chunk to send.
+
+    Don't forget a final call to .flush() to get any final data out!
+
+    """
+
+    def __init__(self, rechunk=False, run_id=None):
+        self.rechunk = rechunk
+        self.is_superrun = run_id and run_id.startswith("_") and not run_id.startswith("__")
+        self.run_id = run_id
+
+        self.cache = None
+
+    def receive(self, chunk):
+        if self.is_superrun:
+            chunk = strax.transform_chunk_to_superrun_chunk(self.run_id, chunk)
+        if not self.rechunk:
+            # We aren't rechunking
+            return chunk
+        if self.cache:
+            # We have an old chunk, so we need to concatenate
+            chunk = strax.Chunk.concatenate([self.cache, chunk])
+        if chunk.data.nbytes >= chunk.target_size_mb * 1e6:
+            # Enough data to send a new chunk!
+            self.cache = None
+            return chunk
+        else:
+            # Not enough data yet, so we cache the chunk
+            self.cache = chunk
+            return None
+
+    def flush(self):
+        result = self.cache
+        self.cache = None
+        return result
