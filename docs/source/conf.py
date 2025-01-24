9e72936cc23012e3b22de16bbee20d5832e7cc89
dachengx <dx2227@columbia.edu>
2025-01-24 00:21:04 -0600

    Drop python 3.9 and loosen requirement of `numpy`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -296,7 +296,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 
 def write_contributing():
